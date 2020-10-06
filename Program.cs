b7823da26cb7ca030743351e35c90136e3a21d78
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2020-10-06 20:31:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static public void my_delegate() 
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.SetCursorPosition(i, i);
+                Console.WriteLine("Sas");
+                Thread.Sleep(1100);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread my_thread = new Thread(my_delegate);
+            Console.SetCursorPosition(15, 15);
+            Console.Write("Waiting...");
+            my_thread.Start();
+            int n = 0;
+            while (my_thread.IsAlive) 
+            {
+                Console.SetCursorPosition(16, 20);
+                Console.Write(n.ToString());
+                n++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
