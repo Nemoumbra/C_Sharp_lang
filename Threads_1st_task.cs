b0c861e755f87c03ab822e4e117a05a8bcf655d4
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2020-10-17 14:51:53 +0300

    First task rewritten

--- Threads_1st_task.cs
+++ Threads_1st_task.cs
@@ -9,80 +9,50 @@ namespace Threads
     class Program
     {
         static object lock1 = new object();
-        static public void my_delegate() 
+        class Twinkler
         {
-            while (true) 
-             {
-                 if (Thread.CurrentThread.Name == "0") //*
-                 {
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(0, 0);
-                         Console.Write("*");
-                     }
-                     Thread.Sleep(1000);
-                     lock (lock1) 
-                     {
-                         Console.SetCursorPosition(0, 0);
-                         Console.Write(" ");
-                     }
-                     Thread.Sleep(1000);
-                 }
-                 else if (Thread.CurrentThread.Name == "1") //@
-                 {
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(5, 0);
-                         Console.Write("@");
-                     }
-                     Thread.Sleep(700);
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(5, 0);
-                         Console.Write(" ");
-                     }
-                     Thread.Sleep(700);
-                 }
-                 else if (Thread.CurrentThread.Name == "2") //#
-                 {
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(0, 5);
-                         Console.Write("#");
-                     }
-                     Thread.Sleep(2000);
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(0, 5);
-                         Console.Write(" ");
-                     }
-                     Thread.Sleep(2000);
-                 }
-                 else //$
-                 {
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(5, 5);
-                         Console.Write("$");
-                     }
-                     Thread.Sleep(1500);
-                     lock (lock1)
-                     {
-                         Console.SetCursorPosition(5, 5);
-                         Console.Write(" ");
-                     }
-                     Thread.Sleep(1500);
-                 }
-             }
+            public int X, Y, sleep_time;
+            public char character;
+            public Twinkler(int Xcoord, int Ycoord, char symbol, int time) 
+            {
+                X = Xcoord;
+                Y = Ycoord;
+                character = symbol;
+                sleep_time = time;
+            }
+            public void twinkle() 
+            {
+                while (true) 
+                {
+                    lock (lock1)
+                    {
+                        Console.SetCursorPosition(X, Y);
+                        Console.Write(character);
+                    }
+                    Thread.Sleep(sleep_time);
+                    lock (lock1)
+                    {
+                        Console.SetCursorPosition(X, Y);
+                        Console.Write(" ");
+                    }
+                    Thread.Sleep(sleep_time);
+                }
+            }
         }
         static void Main(string[] args)
         {
-            for (int i = 0; i < 4; i++)
-            {
-                Thread one_of_four = new Thread(my_delegate);
-                one_of_four.Name = i.ToString();
-                one_of_four.Start();
-            }
+            Twinkler data_asterisk = new Twinkler(0, 0, '*', 1000);
+            Twinkler data_sharp = new Twinkler(0, 5, '#', 2000);
+            Twinkler data_at = new Twinkler(5, 0, '@', 700);
+            Twinkler data_dollar = new Twinkler(5, 5, '$', 1500);
+            Thread asterisk = new Thread(data_asterisk.twinkle);
+            Thread sharp = new Thread(data_sharp.twinkle);
+            Thread at = new Thread(data_at.twinkle);
+            Thread dollar = new Thread(data_dollar.twinkle);
+            asterisk.Start();
+            sharp.Start();
+            at.Start();
+            dollar.Start();
         }
     }
 }
