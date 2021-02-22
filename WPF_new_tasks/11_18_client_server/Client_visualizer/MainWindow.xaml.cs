ebc3fa27dfd7a68617d379cf77d0e52ed46c5635
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2021-02-22 19:37:32 +0300

    Update MainWindow.xaml
    Final version of the visual part of this system.

--- WPF_new_tasks/11_18_client_server/Client_visualizer/MainWindow.xaml.cs
+++ WPF_new_tasks/11_18_client_server/Client_visualizer/MainWindow.xaml.cs
@@ -14,67 +14,142 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
+using System.Windows.Threading;
 
-namespace My_work_12_02_21
+namespace Client
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        //TcpClient client = null;
+        string IP = "";
         void add_message(string msg)
         {
             ListBoxItem temp = new ListBoxItem();
             temp.Content = msg;
             listbox_msg.Items.Add(temp);
         }
-        public MainWindow()
+        void move_dot(string direction) 
         {
-            InitializeComponent();
+            try
+            {
+                switch (direction) 
+                {
+                    case "UP":
+                        {
+                            blue_dot.Margin = new Thickness(blue_dot.Margin.Left, blue_dot.Margin.Top - 10, 0, 0);
+                            break;
+                        }
+                    case "DOWN": 
+                        {
+                            blue_dot.Margin = new Thickness(blue_dot.Margin.Left, blue_dot.Margin.Top + 10, 0, 0);
+                            break;
+                        }
+                    case "LEFT":
+                        {
+                            blue_dot.Margin = new Thickness(blue_dot.Margin.Left - 10, blue_dot.Margin.Top, 0, 0);
+                            break;
+                        }
+                    case "RIGHT":
+                        {
+                            blue_dot.Margin = new Thickness(blue_dot.Margin.Left + 10, blue_dot.Margin.Top, 0, 0);
+                            break;
+                        }
+                }
+            }
+            catch (Exception exept) 
+            {
+                MessageBox.Show(exept.Message, "Error!");
+            }
+        }
+        void get_updates(object sender, EventArgs e)
+        {
             TcpClient client = new TcpClient();
             NetworkStream stream = null;
             string response = "";
             byte[] data = new byte[256];
-            while (true)
+            try
             {
-                try
+                //client = new TcpClient();
+                client.Connect(IP, 8888);
+                data = Encoding.UTF8.GetBytes("GET");
+                stream = client.GetStream();
+                //сделать запрос
+                stream.Write(data, 0, data.Length);
+                response = "";
+                //получить ответ
+                do
                 {
-                    //client = new TcpClient();
-                    client.Connect("127.0.0.1", 8888);
-                    data = Encoding.UTF8.GetBytes("G");
-                    stream = client.GetStream();
-                    //сделать запрос
-                    stream.Write(data, 0, data.Length); 
-                    response = "";
-                    //получить ответ
-                    do
-                    {
-                        int count = stream.Read(data, 0, data.Length);
-                        response += Encoding.UTF8.GetString(data, 0, count);
-                    }
-                    while (stream.DataAvailable);
-                    //обрабатываем ответ
-                    if (response[0] == 'm')
-                    {
-                        add_message(response.Remove(0, 2));
-                    }
-                    if (response.Equals("b1LEFT"))
-                    {
-
-                    }
-                    if (response.Equals("b1RIGHT"))
-                    {
-
-                    }
-                    if (response.Equals("b1UP"))
-                    {
-
-                    }
+                    int count = stream.Read(data, 0, data.Length);
+                    response += Encoding.UTF8.GetString(data, 0, count);
                 }
-                catch (Exception exept)
+                while (stream.DataAvailable);
+                //обрабатываем ответ
+                if (response.StartsWith("ms"))
                 {
-                    MessageBox.Show("Error: " + exept.Message, "Error!");
+                    add_message(response.Remove(0, 2));
                 }
+                if (response.Equals("b1LEFT"))
+                {
+                    move_dot("LEFT");
+                }
+                if (response.Equals("b1RIGHT"))
+                {
+                    move_dot("RIGHT");
+                }
+                if (response.Equals("b1UP"))
+                {
+                    move_dot("UP");
+                }
+                if (response.Equals("b1DOWN"))
+                {
+                    move_dot("DOWN");
+                }
+            }
+            catch (Exception exept)
+            {
+                MessageBox.Show("Error: " + exept.Message, "Error!");
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void start_button_Click(object sender, RoutedEventArgs e)
+        {
+            int interval = 0;
+            try
+            {
+                interval = Convert.ToInt32(textBox2.Text);
+            }
+            catch (Exception exept) 
+            {
+                MessageBox.Show(exept.Message, "Error!");
+                return;
+            }
+            blue_dot.Visibility = Visibility.Visible;
+            chat_label.Visibility = Visibility.Visible;
+            listbox_msg.Visibility = Visibility.Visible;
+            start_button.Visibility = Visibility.Hidden;
+            label1.Visibility = Visibility.Hidden;
+            textBox1.Visibility = Visibility.Hidden;
+            textBox2.Visibility = Visibility.Hidden;
+            label2.Visibility = Visibility.Hidden;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, interval);
+            timer.Tick += get_updates;
+            timer.IsEnabled = true;
+            if (!textBox1.Text.Equals(""))
+            {
+                IP = textBox1.Text;
+            }
+            else 
+            {
+                IP = "127.0.0.1";
             }
         }
     }
