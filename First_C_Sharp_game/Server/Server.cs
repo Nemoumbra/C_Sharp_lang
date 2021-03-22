decb28879ee3b679a2368ed332908e9b26f6b3b1
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2021-03-22 18:53:47 +0300

    Lockless threading server

--- First_C_Sharp_game/Server/Server.cs
+++ First_C_Sharp_game/Server/Server.cs
@@ -28,32 +28,158 @@ namespace Server
     class my_client
     {
         public TcpClient client;
-        public my_client(TcpListener server)
+        public List <string> chat;
+        public List <Circling> remotes;
+        public Random rand;
+        public my_client(server_part server_var/*TcpListener server, List<string> game_chat, List<Circling> players, Random random*/)
         {
-            client = server.AcceptTcpClient();
+            client = server_var.server.AcceptTcpClient();
+            chat = server_var.chat;
+            remotes = server_var.remotes;
+            rand = server_var.rand;
         }
         public void deal_with_client()
         {
-            NetworkStream stream = client.GetStream();
-            string message = "";
-            byte[] data = new byte[256];
-            do 
+            try
             {
-                int bytes = stream.Read(data, 0, data.Length);
-                message += Encoding.UTF8.GetString(data, 0, bytes);
+                NetworkStream stream = client.GetStream();
+                string message = "", response = "";
+                byte[] data = new byte[256];
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    message += Encoding.UTF8.GetString(data, 0, bytes);
+                }
+                while (stream.DataAvailable);
+                string[] words = message.Split(':');
+                if (words[0].Equals("SEND_TEXT"))
+                {
+                    if (remotes.Count - 1 >= Convert.ToInt32(words[1]))
+                    {
+                        chat.Add(words[2]);
+                        data = Encoding.UTF8.GetBytes("OK");
+                        stream.Write(data, 0, data.Length);
+                        stream.Close();
+                        client.Close();
+                        return;       
+                    }
+                }
+                if (words[0].Equals("REG_REMOTE"))
+                {
+                    remotes.Add(new Circling(rand.Next(100), rand.Next(100), 33, Convert.ToByte(rand.Next(255)), Convert.ToByte(rand.Next(255)), Convert.ToByte(rand.Next(255))));
+                    data = Encoding.UTF8.GetBytes((remotes.Count - 1).ToString());
+                    stream.Write(data, 0, data.Length);
+                    stream.Close();
+                    client.Close();
+                    return;
+                }
+                if (words[0].Equals("GET_TEXT"))
+                {
+                    int N = Convert.ToInt32(words[1]);
+                    if (N >= 0 && N <= chat.Count)
+                    {
+                        response = (chat.Count - N).ToString();
+                        for (int i = N; i < chat.Count; i++)
+                        {
+                            response += ":" + chat[i];
+                        }
+                        data = Encoding.UTF8.GetBytes(response);
+                        stream.Write(data, 0, data.Length);
+                        stream.Close();
+                        client.Close();
+                        return;
+                    }
+                }
+                if (words[0].Equals("GET_MESSAGE"))
+                {
+                    int N = Convert.ToInt32(words[1]);
+                    if (N >= 0 && N < chat.Count)
+                    {
+                        response = chat[N];
+                        data = Encoding.UTF8.GetBytes(response);
+                        stream.Write(data, 0, data.Length);
+                        stream.Close();
+                        client.Close();
+                        return;
+                    }
+                }
+                if (words[0].Equals("GET_CIRCLES"))
+                {
+                    response = remotes.Count.ToString();
+                    for (int i = 0; i < remotes.Count; i++)
+                    {
+                        response += ":" + remotes[i].x.ToString();
+                        response += ":" + remotes[i].y.ToString();
+                        response += ":" + remotes[i].d.ToString();
+                        response += ":" + remotes[i].red.ToString();
+                        response += ":" + remotes[i].green.ToString();
+                        response += ":" + remotes[i].blue.ToString();
+                    }
+                    data = Encoding.UTF8.GetBytes(response);
+                    stream.Write(data, 0, data.Length);
+                    stream.Close();
+                    client.Close();
+                    return;
+                }
+                if (words[0].Equals("MOVE_CIRCLE"))
+                {
+                    int N = Convert.ToInt32(words[1]);
+                    if (N <= remotes.Count - 1)
+                    {
+                        switch (words[2])
+                        {
+                            case "UP":
+                                {
+                                    remotes[N].y -= remotes[N].d / 2;
+                                    break;
+                                }
+                            case "DOWN":
+                                {
+                                    remotes[N].y += remotes[N].d / 2;
+                                    break;
+                                }
+                            case "LEFT":
+                                {
+                                    remotes[N].x -= remotes[N].d / 2;
+                                    break;
+                                }
+                            case "RIGHT":
+                                {
+                                    remotes[N].x += remotes[N].d / 2;
+                                    break;
+                                }
+                        }
+                        data = Encoding.UTF8.GetBytes("OK");
+                        stream.Write(data, 0, data.Length);
+                        stream.Close();
+                        client.Close();
+                        return;
+                    }
+                }
+                /*
+                if (words.Length > 1) 
+                {
+                    Console.WriteLine(words[1]);
+                }*/
+                data = Encoding.UTF8.GetBytes("Wrong request!");
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+                client.Close();
             }
-            while (stream.DataAvailable);
-            string[] words = message.Split(':');
-
+            catch (Exception exept) 
+            {
+                Console.WriteLine("Error" + exept.Message);
+            }
         }
     }
-    class my_server
+    class server_part
     {
         public TcpListener server;
+        //public TcpClient client;
         public List<string> chat;
         public List<Circling> remotes;
         public Random rand;
-        public my_server(string ip, int port, int seed)
+        public server_part(string ip, int port, int seed)
         {
             chat = new List<string>();
             remotes = new List<Circling>();
@@ -71,15 +197,40 @@ namespace Server
                 return;
             }
         }
+        /*
+        public void deal_with_client() 
+        {
+            NetworkStream stream = client.GetStream();
+            string message = "", response="";
+            byte[] data = new byte[256];
+            do 
+            {
+                int bytes = stream.Read(data, 0, data.Length);
+                message += Encoding.UTF8.GetString(data, 0, bytes);
+            }
+            while (stream.DataAvailable);
+            string[] words = message.Split(':');
+        }
+        */
         public void process_requests()
         {
             while (true)
             {
                 try
                 {
-                    my_client client = new my_client(server);
+                    /*
+                    my_client client = new my_client(this);
                     Thread client_thread = new Thread(client.deal_with_client);
                     client_thread.Start();
+                    */
+                    
+                    if (server.Pending()) 
+                    {
+                        my_client client = new my_client(this);
+                        Thread client_thread = new Thread(client.deal_with_client);
+                        client_thread.Start();
+                    }
+                    Thread.Sleep(100);
                 }
                 catch (Exception exept)
                 {
@@ -92,13 +243,6 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            /*
-            List<string> chat = null;
-            //List<int> screens = null;
-            List<Circling> remotes = null;
-            TcpListener server = null;
-            Random rand = null;
-            */
             Console.WriteLine("Program started");
 
             string IP = "", port = "";
@@ -116,35 +260,10 @@ namespace Server
                 port = "8888";
                 Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
             }
-            /*
-            try
-            {
-                server = new TcpListener(IPAddress.Parse(IP), Convert.ToInt32(port));
-                server.Start();
-            }
-            catch (Exception exept)
-            {
-                Console.WriteLine("Error: " + exept.Message);
-                Console.ReadKey();
-                return;
-            }*/
-            my_server server = new my_server(IP, Convert.ToInt32(port), 123);
-            //server.start_server(IP, Convert.ToInt32(port));
+            server_part server = new server_part(IP, Convert.ToInt32(port), 123);
             Thread server_thread = new Thread(server.process_requests);
             server_thread.Start();
-            //Console.WriteLine("Server running");
-            /*
-            chat = new List<string>();
-            remotes = new List<Circling>();
-            //screens = new List<int>();
-            rand = new Random(100);
-            TcpClient client = null;
-            string message = "";
-            byte[] data = null;
-            NetworkStream stream = null;
-            string response = "";
-            */
-            }
+            
         }
     }
 }
