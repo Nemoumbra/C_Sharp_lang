4e2ed140a7c775b32cd3bb7f4bc3daff8525bf30
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2021-03-23 20:51:19 +0300

    Up to date remote

--- First_C_Sharp_game/Remote/MainWindow.xaml.cs
+++ First_C_Sharp_game/Remote/MainWindow.xaml.cs
@@ -26,12 +26,14 @@ namespace Client
         byte[] data = null;
         NetworkStream stream = null;
         string key = "", response = "";
+        string IP="", port_str="";
+        int port = 0;
 
-        void hide (UIElement elem)
+        void hide(UIElement elem)
         {
             elem.Visibility = Visibility.Hidden;
         }
-        void show (UIElement elem)
+        void show(UIElement elem)
         {
             elem.Visibility = Visibility.Visible;
         }
@@ -48,7 +50,7 @@ namespace Client
             try
             {
                 client = new TcpClient();
-                client.Connect(ip_textbox.Text, 8888);
+                client.Connect(IP, port);
                 stream = client.GetStream();
                 data = Encoding.UTF8.GetBytes("MOVE_CIRCLE:" + key + ":UP");
                 stream.Write(data, 0, data.Length);
@@ -64,7 +66,7 @@ namespace Client
                     MessageBox.Show("Ошибка: сервер не вернул ответ \"OK\"!");
                 }
                 stream.Close();
-                client.Close(); 
+                client.Close();
             }
             catch (Exception exept)
             {
@@ -77,7 +79,7 @@ namespace Client
             try
             {
                 client = new TcpClient();
-                client.Connect(ip_textbox.Text, 8888);
+                client.Connect(IP, port);
                 stream = client.GetStream();
                 data = Encoding.UTF8.GetBytes("MOVE_CIRCLE:" + key + ":LEFT");
                 stream.Write(data, 0, data.Length);
@@ -106,7 +108,7 @@ namespace Client
             try
             {
                 client = new TcpClient();
-                client.Connect(ip_textbox.Text, 8888);
+                client.Connect(IP, Convert.ToInt32(port_textbox.Text));
                 stream = client.GetStream();
                 data = Encoding.UTF8.GetBytes("MOVE_CIRCLE:" + key + ":DOWN");
                 stream.Write(data, 0, data.Length);
@@ -135,7 +137,7 @@ namespace Client
             try
             {
                 client = new TcpClient();
-                client.Connect(ip_textbox.Text, 8888);
+                client.Connect(IP, port);
                 stream = client.GetStream();
                 data = Encoding.UTF8.GetBytes("MOVE_CIRCLE:" + key + ":RIGHT");
                 stream.Write(data, 0, data.Length);
@@ -166,7 +168,7 @@ namespace Client
                 try
                 {
                     client = new TcpClient();
-                    client.Connect(ip_textbox.Text, 8888);
+                    client.Connect(IP, port);
                     stream = client.GetStream();
                     data = Encoding.UTF8.GetBytes("SEND_TEXT:" + key + ":" + input_textbox.Text);
                     stream.Write(data, 0, data.Length);
@@ -199,15 +201,39 @@ namespace Client
         {
             //попытка подключения к серверу. Если успешно, убрать лишние детали интерфейса, показать новые
             //если неудачно, выдать MessageBox с описанием ошибки
-            if (ip_textbox.Text.Equals(""))
-            {
-                MessageBox.Show("Введите IP адрес сервера!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
+            
             try
             {
                 client = new TcpClient();
-                client.Connect(ip_textbox.Text, 8888);
+                if (localhost_checkbox.IsChecked == true)
+                {
+                    IP = "127.0.0.1";
+                }
+                else 
+                {
+                    if (!ip_textbox.Text.Equals(""))
+                    {
+                        IP = ip_textbox.Text;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введите IP адрес!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        return;
+                    }
+                }
+
+                if (!port_textbox.Text.Equals(""))
+                {
+                    port_str = port_textbox.Text;
+                }
+                else 
+                {
+                    MessageBox.Show("Введите номер порта!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+                port = Convert.ToInt32(port_str);
+
+                client.Connect(IP, port);
                 stream = client.GetStream();
                 data = Encoding.UTF8.GetBytes("REG_REMOTE");
                 stream.Write(data, 0, data.Length);
@@ -225,6 +251,9 @@ namespace Client
                 hide(reg_button);
                 hide(ip_textbox);
                 hide(ip_label);
+                hide(localhost_checkbox);
+                hide(port_textbox);
+                hide(port_label);
                 show(up_button);
                 show(down_button);
                 show(left_button);
@@ -238,5 +267,18 @@ namespace Client
                 MessageBox.Show(exept.Message, "Ошибка!");
             }
         }
+
+        private void checkBox1_Checked(object sender, RoutedEventArgs e)
+        {
+            ip_label.IsEnabled = false;
+            ip_textbox.IsEnabled = false;
+        }
+
+        private void checkBox1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            ip_label.IsEnabled = true;
+            ip_textbox.IsEnabled = true;
+        }
     }
 }
+
